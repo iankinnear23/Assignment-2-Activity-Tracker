37a3c35519426a8267da5695a5514d95abb5d357
ian Kinnear <ian.kinnear@hotmail.com>
2023-07-10 16:16:53 +0100

    changes

--- style.js
+++ style.js
@@ -9,3 +9,26 @@ function openFullImg(pic) {
 function closeFullImg() {
   fullImgBox.style.display = "none";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  const display = document.getElementById("display");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const dateInput = document.getElementById("date").value;
+    const minutesInput = document.getElementById("minutes").value;
+    const stepsInput = document.getElementById("steps").value;
+
+    const logEntry = document.createElement("p");
+    logEntry.textContent = ` Great job, you did a total of ${stepsInput} steps in ${minutesInput} minutes.`;
+    logEntry.style.fontFamily = "Arial, sans-serif";
+    logEntry.style.color = "red";
+
+    display.appendChild(logEntry);
+
+    document.getElementById("date").value = "";
+    document.getElementById("minutes").value = "";
+    document.getElementById("steps").value = "";
+  });
+});
